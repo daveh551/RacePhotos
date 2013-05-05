38e5eeab1e6159d3c4debf997bdd72c141732cc0
Dave Hanna <dave@infotouchsys.com>
2013-05-05 16:12:12 -0500

    Begin adding test for controllers

--- RacePhotos.Tests/Controllers/PhotosController_Test.cs
+++ RacePhotos.Tests/Controllers/PhotosController_Test.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
 using NUnit.Framework;
 using RacePhotos.Controllers;
 using RacePhotosTestSupport;
@@ -14,6 +17,7 @@ namespace RacePhotos.Tests.Controllers
 	{
 		#region SetUp / TearDown
 
+		
 		[SetUp]
 		public void Init()
 		{ }
@@ -33,10 +37,13 @@ namespace RacePhotos.Tests.Controllers
 			//Arrange
 			string expected = "ReturnsRedirectToRacesController";
 			var target = new PhotosController(new FakeDataSource());
+			target.ControllerContext = new ControllerContext(new FakeHttpContext(), new RouteData(), target);
+			var httpContext = target.ControllerContext.HttpContext;
+			
 			//Act
 			var result = target.List();
 			//Assert
-			Assert.AreEqual(expected, result, "failure message");
+			Assert.IsInstanceOf<RedirectToRouteResult>(result);
 		}
 
 		#endregion
