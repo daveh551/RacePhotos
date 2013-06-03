1e0f8127e8deffcb2c1750437098ad5c60eca5ce
Dave Hanna <dave@infotouchsys.com>
2013-06-03 05:34:03 -0500

    Add scaffolding, Races controller

---
+++ RacePhotos/Controllers/RacesController.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotoServer.DataAccessLayer;
+using PhotoServer.Domain;
+using RacePhotos;
+
+namespace RacePhotos.Controllers
+{   
+    public class RacesController : Controller
+    {
+        private IRaceDataSource context = new RaceDataSource();
+
+        //
+        // GET: /Races/
+
+        public ViewResult Index()
+        {
+            return View(context.Races.Include(race => race.Event).Include(race => race.Distance).ToList());
+        }
+
+        //
+        // GET: /Races/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Race race = context.Races.Single(x => x.Id == id);
+            return View(race);
+        }
+
+        //
+        // GET: /Races/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.PossibleEvents = context.Events;
+            ViewBag.PossibleDistances = context.Distances;
+            return View();
+        } 
+
+        //
+        // POST: /Races/Create
+
+        [HttpPost]
+        public ActionResult Create(Race race)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Races.Add(race);
+                context.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.PossibleEvents = context.Events;
+            ViewBag.PossibleDistances = context.Distances;
+            return View(race);
+        }
+        
+        //
+        // GET: /Races/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Race race = context.Races.Single(x => x.Id == id);
+            ViewBag.PossibleEvents = context.Events;
+            ViewBag.PossibleDistances = context.Distances;
+            return View(race);
+        }
+
+        //
+        // POST: /Races/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Race race)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(race).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PossibleEvents = context.Events;
+            ViewBag.PossibleDistances = context.Distances;
+            return View(race);
+        }
+
+        //
+        // GET: /Races/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Race race = context.Races.Single(x => x.Id == id);
+            return View(race);
+        }
+
+        //
+        // POST: /Races/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Race race = context.Races.Single(x => x.Id == id);
+            context.Races.Remove(race);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
